72560e7c35c999709038d2d427a62cc42c5f61fb
Tochi Onuegbu <tochi.onuegbu@duke.edu>
2023-09-21 15:15:35 -0400

    removed particle from blog

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,93 +1,6 @@
 import DarkModeButton from "~/components/DarkModeButton";
-import { useCallback } from "react";
-import type { Container, Engine } from "tsparticles-engine";
-import Particles from "react-tsparticles";
-//import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
-import { loadSlim } from "tsparticles-slim";
 
-const App = () => {
-    const particlesInit = useCallback(async (engine: Engine) => {
-        console.log(engine);
-  
-        // you can initialize the tsParticles instance (engine) here, adding custom shapes or presets
-        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-        // starting from v2 you can add only the features you need reducing the bundle size
-        //await loadFull(engine);
-        await loadSlim(engine);
-    }, []);
-  
-    const particlesLoaded = useCallback(async (container: Container | undefined) => {
-        await console.log(container);
-    }, []);
-    return (
-        <Particles
-            id="tsparticles"
-            init={particlesInit}
-            loaded={particlesLoaded}
-            options={{
-                
-                fpsLimit: 120,
-                interactivity: {
-                    events: {
-                        onHover: {
-                            enable: true,
-                            mode: "repulse",
-                        },
-                        resize: true,
-                    },
-                    modes: {
-                        push: {
-                            quantity: 4,
-                        },
-                        repulse: {
-                            distance: 50,
-                            duration: 0.4,
-                        },
-                    },
-                },
-                particles: {
-                    color: {
-                        value: "#474747",
-                    },
-                    links: {
-                        color: "#838282",
-                        distance: 150,
-                        enable: true,
-                        opacity: 0.10,
-                        width: 1,
-                    },
-                    move: {
-                        direction: "none",
-                        enable: true,
-                        outModes: {
-                            default: "bounce",
-                        },
-                        random: false,
-                        speed: 0.4,
-                        straight: false,
-                    },
-                    number: {
-                        density: {
-                            enable: true,
-                            area: 800,
-                        },
-                        value: 80,
-                    },
-                    opacity: {
-                        value: 0.09,
-                    },
-                    shape: {
-                        type: "circle",
-                    },
-                    size: {
-                        value: { min: 1, max: 3 },
-                    },
-                },
-                detectRetina: true,
-            }}
-        />
-    );
-  };
+
 
 export default function Blog() {
     return (
@@ -97,7 +10,6 @@ export default function Blog() {
 
             <div className="mt-2 pt-16 pl-24">
                 <DarkModeButton />
-                <App />
 
             </div>
 
